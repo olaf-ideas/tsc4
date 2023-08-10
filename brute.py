dfc5cdbdf2f614b7ab3007b1ff8426ec8ce0e026
olafideas <49076188+olaf-ideas@users.noreply.github.com>
2023-08-10 11:22:51 +0000

    Task5: binary lifting for fibonacci

--- brute.py
+++ brute.py
@@ -11,20 +11,34 @@ def fib_fast(n):
 
     while n > 0:
         if n % 2 == 1:
+            print("hee")
             bq = b * q
             b = a * q + bq + b * p
             a = a * p + bq
         
-        qq = q * q
-        q = 2 * p * q + qq
-        p = p * p + qq
+        if n > 1:
+            print("hmm")
+            qq = q * q
+            q = 2 * p * q + qq
+            p = p * p + qq
 
+        print(a, b, p, q)
+
+        assert p < 2**257
+        assert q < 2**257
+        assert a < 2**257
+        assert b < 2**257
+        
         n >>= 1
 
     return a, b
 
-for i in range(80):
+for i in range(370):
+    assert(fib(i) == fib_fast(i)[0])
     print(fib(i), fib_fast(i))
+
+print ("testing big")
 
 print(fib(369))
 print(fib_fast(369))
+# print(fib_fast(370))
