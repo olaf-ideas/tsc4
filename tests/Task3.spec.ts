2acf54b14644274393cb18451bf83b480370c290
olafideas <49076188+olaf-ideas@users.noreply.github.com>
2023-08-11 11:46:45 +0000

    Task3: next try pls give me some test, at least one

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -37,38 +37,78 @@ describe('Task3', () => {
         // blockchain and task3 are ready to use
     });
 
-    it('should work on this simple test', async () => {
+    function solve(flag: string, value: string, list: string) {
+        return list.replaceAll(flag.toString(2), value.toString(2));
+    };
 
-        let len = 4 * 5;
+    it('should work on this simple test', async () => {
 
         let linked_list = beginCell();
         
-        let list = '';
+        let list = '011101110';
 
-        for (let i = 0; i < len; i++) {
+        for (let i = 0; i < list.length; i++) {
 
-            let value: number = i % 2;
-            list += value.toString();
-
-            linked_list.storeBit(value);
+            linked_list.storeBit(Number(list.at(i)));
         }
 
         let linked_list_cell = linked_list.endCell();
 
-        let flag =  0b1010n;
-        let value = 0b1001n;
+        let flag =  0b111n;
+        let value = 0b1n;
 
-        let hexToBin = require('hex-to-binary');
-
-        console.log("input : ", list.toString());
+        console.log("input : ", list);
         console.log("flag: ", flag);
         console.log("value: ", value);
         console.log("cell: ", linked_list_cell.toString());
 
         const result = await task3.getFindAndReplace(flag, value, linked_list_cell);
 
-        console.log("bits: ", result.bits.toString());
-        console.log("result: ", hexToBin(result.bits.toString()));
+        let result_list = '';
 
+        for (let i = 0; i < result.bits.length; i++) {
+            result_list += Number(result.bits.at(i))
+        }
+
+        expect(result_list).toEqual(solve(flag.toString(2), value.toString(2), list));
+    });
+
+    it('should work on this hard test', async () => {
+
+        for (let rep = 0; rep < 100; rep++) {
+
+            let linked_list = beginCell();
+            
+            let list = ''
+
+            for (let i = 0; i < 512; i++) {
+                let x = Math.round(Math.random());
+
+                list += x.toString();
+
+                linked_list.storeBit(Number(list.at(i)));
+            }
+
+            let linked_list_cell = linked_list.endCell();
+
+            let flag =  Math.round(Math.random() * 10000);
+            let value = Math.round(Math.random() * 10000)
+
+            console.log("input : ", list);
+            console.log("flag: ", flag.toString(2));
+            console.log("value: ", value.toString(2));
+            console.log("cell: ", linked_list_cell.toString());
+
+            const result = await task3.getFindAndReplace(BigInt(flag), BigInt(value), linked_list_cell);
+
+            let result_list = '';
+
+            for (let i = 0; i < result.bits.length; i++) {
+                result_list += Number(result.bits.at(i))
+            }
+
+            expect(result_list).toEqual(solve(flag.toString(2), value.toString(2), list));
+
+        }
     });
 });
