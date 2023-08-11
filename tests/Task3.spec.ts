9f7e5ce29c7822cf012d93efd57a7e16f5f3c119
olafideas <49076188+olaf-ideas@users.noreply.github.com>
2023-08-11 12:14:52 +0000

    Task3: fixed unthinkable bug

--- tests/Task3.spec.ts
+++ tests/Task3.spec.ts
@@ -38,7 +38,7 @@ describe('Task3', () => {
     });
 
     function solve(flag: string, value: string, list: string) {
-        return list.replaceAll(flag.toString(2), value.toString(2));
+        return list.replaceAll(flag, value);
     };
 
     it('should work on this simple test', async () => {
@@ -57,10 +57,10 @@ describe('Task3', () => {
         let flag =  0b111n;
         let value = 0b1n;
 
-        console.log("input : ", list);
-        console.log("flag: ", flag);
-        console.log("value: ", value);
-        console.log("cell: ", linked_list_cell.toString());
+        // console.log("input : ", list);
+        // console.log("flag: ", flag);
+        // console.log("value: ", value);
+        // console.log("cell: ", linked_list_cell.toString());
 
         const result = await task3.getFindAndReplace(flag, value, linked_list_cell);
 
@@ -75,7 +75,7 @@ describe('Task3', () => {
 
     it('should work on this hard test', async () => {
 
-        for (let rep = 0; rep < 100; rep++) {
+        for (let rep = 0; rep < 10; rep++) {
 
             let linked_list = beginCell();
             
@@ -91,13 +91,13 @@ describe('Task3', () => {
 
             let linked_list_cell = linked_list.endCell();
 
-            let flag =  Math.round(Math.random() * 10000);
-            let value = Math.round(Math.random() * 10000)
+            let flag =  0b1011101;
+            let value = 0b1110011;
 
-            console.log("input : ", list);
-            console.log("flag: ", flag.toString(2));
-            console.log("value: ", value.toString(2));
-            console.log("cell: ", linked_list_cell.toString());
+            // console.log("input : ", list);
+            // console.log("flag: ", flag.toString(2));
+            // console.log("value: ", value.toString(2));
+            // console.log("cell: ", linked_list_cell.toString());
 
             const result = await task3.getFindAndReplace(BigInt(flag), BigInt(value), linked_list_cell);
 
@@ -110,5 +110,132 @@ describe('Task3', () => {
             expect(result_list).toEqual(solve(flag.toString(2), value.toString(2), list));
 
         }
+    });
+
+    it('should work on this linked list x2', async () => {
+
+        let linked_list1 = beginCell();
+        let linked_list2 = beginCell();
+        
+        let list = ''
+
+        for (let i = 0; i < 12; i++) {
+            let x = (i * 21 + 11) % 2;
+
+            list += x.toString();
+
+            linked_list1.storeBit(x);
+        }
+
+        for (let i = 0; i < 20; i++) {
+            let x = (i * 10 + 11) % 2;
+
+            list += x.toString();
+
+            linked_list2.storeBit(x);
+        }
+
+        let linked_list_cell2 = linked_list2.endCell();
+
+        linked_list1.storeRef(linked_list_cell2);
+
+        let linked_list_cell1 = linked_list1.endCell();
+
+        let flag =  0b1011;
+        let value = 0b1001;
+
+        console.log("input : ", list);
+        console.log("flag: ", flag.toString(2));
+        console.log("value: ", value.toString(2));
+        console.log("cell 1: ", linked_list_cell1.toString());
+        console.log("cell 2: ", linked_list_cell2.toString());
+
+        const answer = solve(flag.toString(2), value.toString(2), list);
+
+        const result = await task3.getFindAndReplace(BigInt(flag), BigInt(value), linked_list_cell1);
+
+        let result_list = '';
+
+        for (let i = 0; i < result.bits.length; i++) {
+            result_list += Number(result.bits.at(i));
+        }
+
+        result_list += '|'
+
+        let neigh = result.refs.at(0) ?? result;
+
+        for (let i = 0; i < neigh.bits.length; i++) {
+            result_list += Number(neigh.bits.at(i));
+        }
+
+        console.log("result: ", result_list);
+        console.log("answer: ", answer);
+
+        // expect(result_list).toEqual(solve(flag.toString(2), value.toString(2), list));
+    });
+
+    it('should work on this linked list x2 hard', async () => {
+
+        let linked_list1 = beginCell();
+        let linked_list2 = beginCell();
+        
+        let list = ''
+
+        let cellA = '10100001111';
+        for (let i = 0; i < cellA.length; i++) {
+            let x = Number(cellA[i])
+
+            list += x.toString();
+
+            linked_list1.storeBit(x);
+        }
+
+        let cellB = '11111000111111';
+        for (let i = 0; i < cellB.length; i++) {
+            
+            let x = Number(cellB[i]);
+
+            list += x.toString();
+
+            linked_list2.storeBit(x);
+        }
+
+        let linked_list_cell2 = linked_list2.endCell();
+
+        linked_list1.storeRef(linked_list2);
+
+        let linked_list_cell1 = linked_list1.endCell();
+
+        let flag =  0b101110101;
+        let value = 0b111111111;
+
+        console.log("input : ", list);
+        console.log("flag: ", flag.toString(2));
+        console.log("value: ", value.toString(2));
+        console.log("cell 1: ", linked_list_cell1.toString());
+        console.log("cell 2: ", linked_list_cell2.toString());
+
+        const answer = solve(flag.toString(2), value.toString(2), list);
+
+        const result = await task3.getFindAndReplace(BigInt(flag), BigInt(value), linked_list_cell1);
+
+        let result_list = '';
+
+        for (let i = 0; i < result.bits.length; i++) {
+            result_list += Number(result.bits.at(i));
+        }
+
+        let neigh = result.refs.at(0) ?? result;
+
+        result_list += '|';
+
+        for (let i = 0; i < neigh.bits.length; i++) {
+            result_list += Number(neigh.bits.at(i));
+        }
+
+        console.log("result: ", result_list);
+        console.log("answer: ", answer);
+
+        // expect(result_list).toEqual(solve(flag.toString(2), value.toString(2), list));
     });
 });
